4a59773ae17a291960beade567845e528eca3df5
Ankush Gupta <ankush.me@hotmail.com>
2013-04-26 16:11:32 -0700

    started writing quad edge structure

---
+++ src/qedge/QuadEdge.h
@@ -0,0 +1,18 @@
+/**
+ * Author: Ankush Gupta
+ * Date  : 26 April 2013
+ *
+ * Defines the quad-edge structure as described in Guibas and Stolfi
+ * for representing subdivisions of manifolds.
+ *
+ *  SIMPLIFICATION : Assumes that the manifold is orientable.
+ *                   Hence only one orientation is represented.*/
+
+#ifndef __QUADEDGE_H__
+#define __QUADEDGE_H__
+
+
+
+
+
+#endif //__QUADEDGE_H__
