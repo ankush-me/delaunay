3b9c1c798c10c0beb387360587f5383625f5b9cc
Ankush Gupta <ankush.me@hotmail.com>
2013-04-28 04:38:02 -0700

    tested sorting. works

--- src/test/test_read_node.cpp
+++ src/test/test_read_node.cpp
@@ -1,4 +1,12 @@
+/**********************************
+ *  Small test to test
+ *     1. Reading .node files.
+ *     2. lexico-graphic sorting.
+ **********************************/
+
 #include "qedge/io_utils.h"
+#include "utils/sorting.h"
+
 #include <Eigen/AlignedVector>
 
 using namespace Eigen;
@@ -7,15 +15,28 @@ using namespace std;
 int main (int argc, char* argv[]) {
 
 	string files[] = {"spiral.node",
-			          "box.node",
-			          "grid.node",
-			          "flag.node"};
+			"box.node",
+			"grid.node",
+			"flag.node"};
 
-	string fname = string(EXPAND (PROJECT_DATA_DIR)) + "/" +  files[2];
+	string fname = string(EXPAND (PROJECT_DATA_DIR)) + "/" +  files[0];
 	vector2d points;
 	readNodeFile(fname, points);
 
 	cout << "Number of points : " << points.size()<<endl;
+	for (int i=0; i < points.size(); i++ ) {
+		cout << "    ["<<points[i].transpose()<<"]"<<endl;
+	}
+
+	// test sorting.
+	int min_idx = lexicoMin(points);
+	int max_idx = lexicoMax(points);
+
+	cout << "LexicoMin point : "<<points[min_idx].transpose()<<endl;
+	cout << "LexicoMax point : "<<points[max_idx].transpose()<<endl;
+
+	lexicoSort(points);
+	cout << "Sorted points : "<<endl;
 	for (int i=0; i < points.size(); i++ ) {
 		cout << "    ["<<points[i].transpose()<<"]"<<endl;
 	}
--- src/utils/sorting.h
+++ src/utils/sorting.h
@@ -1,26 +1,35 @@
-#pragma once
+#ifndef __SORTING_H__
+#define __SORTING_H__
 
 #include <Eigen/Dense>
 #include <Eigen/AlignedVector>
 #include <vector>
-
 #include <iostream>
+#include <algorithm>
 
-struct Comparator : std::binary_function <Eigen::VectorXf, Eigen::VectorXf, bool> {
+
+// compare points lexico-graphically
+struct Comparator : std::binary_function <Eigen::VectorXd, Eigen::VectorXd, bool> {
 	int d; // dimensions of the vector
 	Comparator(int _d) : d(_d) {}
 
-	bool operator() (const Eigen::VectorXf &v1, const Eigen::VectorXf &v2) const;
+	bool operator() (const Eigen::VectorXd &v1, const Eigen::VectorXd &v2) const {
+		int i = 0;
+		while(i < d && v1[i] == v2[i]) {i++;}
+		return (i==d)? false :  v1[i] < v2[i];
+	}
 };
 
-
-struct EqComparator : std::binary_function <Eigen::VectorXf, Eigen::VectorXf, bool> {
+struct EqComparator : std::binary_function <Eigen::VectorXd, Eigen::VectorXd, bool> {
 	int d; // dimensions of the vector
 	EqComparator(int _d) : d(_d) {}
 
-	bool operator() (const Eigen::VectorXf &v1, const Eigen::VectorXf &v2) const;
+	bool operator() (const Eigen::VectorXd &v1, const Eigen::VectorXd &v2) const {
+		int i = 0;
+		while(i < d && v1[i] == v2[i]) {i++;}
+		return (i==d);
+	}
 };
-
 
 /** Templatized comparator for lexicographic less-than test.
  *  The comparisons are made of the indices based on the points stores in PTS.*/
@@ -43,43 +52,54 @@ public:
 	}
 };
 
-
-
-/** Sort a matrix NxD (N points in D dimensions) lexicographically. In-place. */
-void lexicoSort(Eigen::MatrixXf &mat_nd);
-
-
 /** Sort a vector of points lexicographically. In-place. */
-template <typename T, int R, int C, int _Options, int _MaxRows, int _MaxCols>
-void lexicoSort(std::vector<Eigen::Matrix<T, R,C,  _Options, _MaxRows, _MaxCols> > &mat_nd);
-
 /** Sort a vector of points lexicographically. In-place. Redefined for aligned-vectors.*/
 template <typename T, int R, int C, int _Options, int _MaxRows, int _MaxCols>
 void lexicoSort(std::vector<Eigen::Matrix<T, R, C, _Options, _MaxRows, _MaxCols>,
-		Eigen::aligned_allocator<Eigen::Matrix<T, R, C, _Options, _MaxRows, _MaxCols> > >& mat_nd);
-
-
+		Eigen::aligned_allocator<Eigen::Matrix<T, R, C, _Options, _MaxRows, _MaxCols> > >& mat_nd) {
+	int n = mat_nd.size();
+	if (n != 0) {
+		int d = mat_nd[0].size();
+		sort(mat_nd.begin(), mat_nd.end(), Comparator(d));
+	}
+}
 
 /** Returns the index of the lexicographically MINIMUM point in a list of points.*/
-int lexicoMin(const Eigen::MatrixXf &mat_nd);
-
-template <typename T, int R, int C, int _Options, int _MaxRows, int _MaxCols>
-int lexicoMin(const std::vector<Eigen::Matrix<T, R,C,  _Options, _MaxRows, _MaxCols> > &mat_nd);
-
 template <typename T, int R, int C, int _Options, int _MaxRows, int _MaxCols>
 int lexicoMin(const std::vector<Eigen::Matrix<T, R, C, _Options, _MaxRows, _MaxCols>,
-		Eigen::aligned_allocator<Eigen::Matrix<T, R, C, _Options, _MaxRows, _MaxCols> > >& mat_nd);
+		Eigen::aligned_allocator<Eigen::Matrix<T, R, C, _Options, _MaxRows, _MaxCols> > >& mat_nd) {
+	const int n = mat_nd.size();
+	if (!n) return -1;
 
+	const int d = mat_nd[0].size();
+	Comparator isLess(d);
 
+	int min_idx = 0;
+
+	for (int i= 1; i < n; i += 1) {
+		if (isLess(mat_nd[i], mat_nd[min_idx]) )
+			min_idx = i;
+	}
+	return min_idx;
+}
 
 /** Returns the index of the lexicographically MAXIMUM point in a list of points.*/
-int lexicoMax(const Eigen::MatrixXf &mat_nd);
-
-template <typename T, int R, int C, int _Options, int _MaxRows, int _MaxCols>
-int lexicoMax(const std::vector<Eigen::Matrix<T, R,C,  _Options, _MaxRows, _MaxCols> > &mat_nd);
-
 template <typename T, int R, int C, int _Options, int _MaxRows, int _MaxCols>
 int lexicoMax(const std::vector<Eigen::Matrix<T, R, C, _Options, _MaxRows, _MaxCols>,
-		Eigen::aligned_allocator<Eigen::Matrix<T, R, C, _Options, _MaxRows, _MaxCols> > >& mat_nd);
+		Eigen::aligned_allocator<Eigen::Matrix<T, R, C, _Options, _MaxRows, _MaxCols> > >& mat_nd) {
+	const int n = mat_nd.size();
+	if (!n) return -1;
 
+	const int d = mat_nd[0].size();
+	Comparator isLess(d);
 
+	int min_idx = 0;
+
+	for (int i= 1; i < n; i += 1) {
+		if (!isLess(mat_nd[i], mat_nd[min_idx]) )
+			min_idx = i;
+	}
+	return min_idx;
+}
+
+#endif
